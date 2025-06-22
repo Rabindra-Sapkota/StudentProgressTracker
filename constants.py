462bbcfb6c8a2b55ade952a210f839785fb30646
Rabindra Sapkota <rabindrasapkota2@gmail.com>
2025-06-23 00:01:51 +0545

    Added constants for project

---
+++ constants.py
@@ -0,0 +1,35 @@
+EXERCISES = [
+    "Basic",
+    "String",
+    "Input Output",
+    "Formatting",
+    "Conditional",
+    "List",
+    "Data Structure",
+    "Set",
+    "Dictionary",
+    "Loop",
+    "Function",
+    "File Handling",
+    "Exception",
+    "OOP",
+    "Library",
+    "Database",
+    "Samachar Sangraha",
+    "Progress Tracker",
+]
+
+MAIL_SUBJECT = "Python Class Progress Update"
+MAIL_BODY = """
+Hello {student_name},
+
+Your progress is {progress}%.
+
+Completed Exercises: {completed}.
+Incomplete Exercises: {incomplete}.
+
+Please complete the pending exercises for which theory is completed.
+
+Regards,
+Rabindra
+"""
